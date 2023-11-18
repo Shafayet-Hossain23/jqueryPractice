698fbab7ac947ad937fd3583adb2996e4814ad7f
Shafayet-Hossain23 <mdshafayethossain6530@gmail.com>
2023-11-18 12:29:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// $("h1").text("sifat")
+// $("span").parent().css({ "border": "1px solid red" });
+// $("span").parents().css({ "border": "1px solid red" });
+// $("span").parentsUntil("div").css({ "border": "1px solid red" });
+// $("li").children().css({ "border": "1px solid red" });
+// $(".span1").siblings().css({ "border": "1px solid red" });
+// $(".span1").nextUntil(".atika").css({ "border": "1px solid red" });
+// $("h1").html("<i>Hi how are you?</i>")
+// $("h1").append(" Atik")
+// $("h1").prepend(" Atik! ")
+// $("button").click(function () {
+//     // $("h1").text("I am fine.")
+//     // $("h1").addClass("style1")
+//     // $("h1").toggleClass("style1")
+//     // $("h1").hide()
+//     // $("h1").show()
+//     $("h1").toggle(2000)
+
+
+// })
+// $("button").on("keyup", function () {
+//     $("h1").toggle(2000)
+// })
+
+
